71061e42faa8b4e8758cb15c264f825e35ac440f
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2020-08-05 14:59:13 +0200

    svelte material 🎉 and more styling 😊

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,109 @@
 <script>
     import { currentRoll, playable } from "./stores.js";
     import Player from "./Components/Player.svelte";
-    let roll = 10;
+    import Card, { Content, PrimaryAction, Actions } from "@smui/card";
+    import Button, { Label } from "@smui/button";
+    import HelperText from "@smui/textfield/helper-text/index";
+    import FloatingLabel from "@smui/floating-label";
+    import Textfield, { Input, Textarea } from "@smui/textfield";
+    import { Icon } from "@smui/common";
+    import GiCrownedSkull from "svelte-icons/gi/GiCrownedSkull.svelte";
+    import MdArrowForward from "svelte-icons/md/MdArrowForward.svelte";
+    let roll = "";
+    let invalid = false;
+    let input = undefined;
 </script>
 
 <style>
-    main {
-        text-align: center;
-        height: 100%;
-        width: 100%;
-    }
+
 </style>
 
-<main>
+<div class="container flex-column">
     {#if $playable}
         <Player upsideDown={true} />
         <Player upsideDown={false} />
     {:else}
-        <h1>Deathroll</h1>
-        <i />
-        <input type="number" bind:value={roll} />
-        <button
-            on:click={() => {
-                currentRoll.set(roll);
-                playable.set(true);
-            }}>
-            Start rolling
-        </button>
+        <div class="card-container">
+            <Card style="width: 320px; text-align:center">
+                <PrimaryAction on:click={() => input.focus()}>
+                    <Content>
+                        <div class="icon" style="margin: 0 auto;">
+                            <GiCrownedSkull />
+                        </div>
+                        <h1>Deathroll</h1>
+                        <div style="margin: 2vh 0;">
+                            <Textfield
+                                bind:this={input}
+                                class="shaped-outlined"
+                                variant="outlined"
+                                invalid={invalid ? true : false}
+                                bind:value={roll}
+                                label="Enter a number"
+                                input$aria-controls="helper-text-shaped-outlined-a"
+                                input$aria-describedby="helper-text-shaped-outlined-a" />
+                            <HelperText id="helper-text-shaped-outlined-a">
+                                {invalid ? 'Has to be a number!' : 'Number to start rolling from'}
+                            </HelperText>
+                        </div>
+
+                    </Content>
+                </PrimaryAction>
+                <Actions fullBleed>
+                    <Button
+                        color="secondary"
+                        variant="raised"
+                        style="padding: 1.5vh"
+                        on:click={() => {
+                            if (Number(roll)) {
+                                invalid = false;
+                                currentRoll.set(roll);
+                                playable.set(true);
+                            } else {
+                                input.focus();
+                                invalid = true;
+                            }
+                        }}>
+                        <Label>Start rolling</Label>
+                        <Icon>
+                            <MdArrowForward />
+                        </Icon>
+                    </Button>
+                </Actions>
+            </Card>
+        </div>
     {/if}
-</main>
+</div>
+<!-- <div class="icon">
+    <GiCrownedSkull />
+</div>
+<h1>Deathroll</h1>
+<div style="margin: 2vh 0;">
+    <Textfield
+        class="shaped-outlined"
+        variant="outlined"
+        invalid={invalid ? true : false}
+        bind:value={roll}
+        label="Enter a number"
+        input$aria-controls="helper-text-shaped-outlined-a"
+        input$aria-describedby="helper-text-shaped-outlined-a" />
+    <HelperText id="helper-text-shaped-outlined-a">
+        {invalid ? 'Has to be a number!' : 'Number to start rolling from'}
+    </HelperText>
+</div>
+<Button
+    variant="raised"
+    style="padding: 1.5vh"
+    on:click={() => {
+        if (Number(roll)) {
+            invalid = false;
+            currentRoll.set(roll);
+            playable.set(true);
+        } else {
+            invalid = true;
+        }
+    }}>
+    <Icon>
+        <GiRollingDices />
+    </Icon>
+    <Label>Start rolling</Label>
+</Button> -->
--- src/Components/Player.svelte
+++ src/Components/Player.svelte
@@ -1,5 +1,6 @@
 <script>
     export let upsideDown;
+    import Button from "@smui/button";
     import { currentRoll, player1, playable } from "../stores.js";
     function rollDice() {
         const newNumber = Math.floor(Math.random() * $currentRoll - 1) + 1;
@@ -22,104 +23,52 @@
 </script>
 
 <style>
-    .player1,
-    .player2 {
-        height: 50%;
-        width: 100%;
-    }
-    .player1 {
-        background-color: white;
-        color: black;
-        position: relative;
-    }
-    .player1 button {
-        position: absolute;
-        background-color: blue;
-        outline: none;
-        border: none;
-        bottom: 0;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 50%;
-        height: 20%;
-    }
-    .player1 button:disabled,
-    .player2 button:disabled {
-        background-color: green;
-    }
-    .player1 .text-up {
-        color: black;
-        -moz-transform: translate(50%, 50%); /* Firefox */
-        -webkit-transform: translate(50%, 50%); /* Webkit */
-        -ms-transform: translate(50%, 50%); /* IE */
-        -o-transform: translate(50%, 50%); /* Opera */
-        transform: translate(50%, 50%); /* future */
-        position: absolute;
-    }
-    .player1 .text-up.roll-count {
-        right: 50%;
-    }
-    .player2 {
-        background-color: black;
-        color: white;
-        position: relative;
-        /* transform: rotateY(180deg); */
-    }
-    .player2 button {
-        position: relative;
-        background-color: blue;
-        outline: none;
-        border: none;
-        width: 50%;
-        height: 20%;
-    }
-    .player2 .text-down {
-        color: white;
-        -moz-transform: rotate(-180deg) translate(50%, 50%); /* Firefox */
-        -webkit-transform: rotate(-180deg) translate(50%, 50%); /* Webkit */
-        -ms-transform: rotate(-180deg) translate(50%, 50%); /* IE */
-        -o-transform: rotate(-180deg) translate(50%, 50%); /* Opera */
-        transform: rotate(-180deg) translate(50%, 50%); /* future */
-        position: absolute;
-    }
-    .player2 .text-down.roll-count {
-        bottom: 0;
-        right: 50%;
-        transform: rotate(-180deg) translate(-50%, 50%);
-    }
+
 </style>
 
 <div class={upsideDown ? 'player2' : 'player1'}>
     {#if upsideDown && $player1}
-        <button
+        <Button
+            class="roll-button"
+            variant="raised"
+            color="secondary"
             disabled
             on:click={() => {
                 rollDice();
             }}>
             <span class={upsideDown ? 'text-down' : 'text-up'}>Player 2</span>
-        </button>
+        </Button>
     {:else if upsideDown && !$player1}
-        <button
+        <Button
+            class="roll-button"
+            variant="raised"
+            color="secondary"
             on:click={() => {
                 rollDice();
             }}>
             <span class={upsideDown ? 'text-down' : 'text-up'}>Player 2</span>
-        </button>
+        </Button>
     {:else if $player1}
-        <button
+        <Button
+            class="roll-button"
+            variant="raised"
+            color="primary"
             on:click={() => {
                 rollDice();
             }}>
             <span class={upsideDown ? 'text-down' : 'text-up'}>Player 1</span>
-        </button>
+        </Button>
     {:else}
-        <button
+        <Button
+            class="roll-button"
+            variant="raised"
+            color="primary"
             disabled
             on:click={() => {
                 rollDice();
             }}>
             <span class={upsideDown ? 'text-down' : 'text-up'}>Player 1</span>
-        </button>
+        </Button>
     {/if}
 
     <h1 class={upsideDown ? 'text-down roll-count' : 'text-up roll-count'}>
